27c893787b0c72db88178fb542d87d0dc9a030d1
David-Angel-M <david.angelm28@gmail.com>
2022-11-16 18:45:42 +0000

    Add words class

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import requests
+
+class Words:
+    """
+    Words manager
+    """
+
+    def __init__(self):
+        self.words = []
+
+    def getWordList(self):
+        word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+        response = requests.get(word_site, timeout=10)
+        wordsInBytes = response.content.splitlines()
+        self.convertBytesIntoString(wordsInBytes)
+        self.filterWordList()
+
+    def convertBytesIntoString(self, wordsInBytes):
+        for word in wordsInBytes:
+            self.words.append(word.decode("utf-8"))
+
+    def filterWordList(self):
+        for word in self.words:
+            if len(word) < 4:
+                self.words.remove(word)
+
+    def getRandomWord(self):
+        index = random.randint(0, len(self.words) - 1)
+        return self.words[index]
+
